6c84e5060e34b0a2dbfdfcd400b2cc577c17a444
Nguyen The Minh <s3878434@rmit.edu.vn>
2021-08-31 15:09:36 +0700

    fix data

--- data/product.sql
+++ data/product.sql
@@ -5,10 +5,11 @@ CREATE TABLE `product` (
   `closingtime` DATETIME NOT NULL,
   `sellerid` int(100) NOT NULL,
   `buyerid` int(100),
+  `bidplaced` int(100),
   PRIMARY KEY(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 SET time_zone = '+07:00'
-INSERT INTO `product` (`name`, `id`, `minimumprice`, `closingtime`, `sellerid`, `buyerid`) VALUES
-("rolex", 1, 15000000, '2021-08-31', 12341245 , 2147483647)
-("drone", 2, 3000000, '2021-08-32', 2147483647, 12341245)
-("rolex", 3, 15000000, '2021-08-31', 12341245, 2147483647)
+INSERT INTO `product` (`name`, `id`, `minimumprice`, `closingtime`, `sellerid`, `buyerid`, `bidplaced`) VALUES
+("rolex", 1, 15000000, '2021-08-31', 12341245 , 2147483647, 1)
+("drone", 2, 3000000, '2021-08-32', 2147483647, 12341245, 2)
+("rolex", 3, 15000000, '2021-08-31', 12341245, 2147483647, 1)
