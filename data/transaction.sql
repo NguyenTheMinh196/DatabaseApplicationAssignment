78ff9740bc6504f74e604b41af92ce7bdb892e68
Nguyen The Minh <s3878434@rmit.edu.vn>
2021-09-06 23:41:14 +0700

    fix transaction

--- data/transaction.sql
+++ data/transaction.sql
@@ -1,4 +1,5 @@
-CREATE VIEW TRANSACTION AS
-SELECT id, name, closingtime, minimumprice AS price, sellerid, buyerid FROM product P;
+
 ALTER TABLE `product` ADD `status` BIT(1) NOT NULL AFTER `bidplaced`;
+CREATE VIEW TRANSACTION AS
+SELECT id, name, closingtime, minimumprice AS price, sellerid, buyerid, status FROM product P;
 -- change all the product that considered past into number 1.
