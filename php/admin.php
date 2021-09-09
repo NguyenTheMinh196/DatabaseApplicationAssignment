faec34b0058df2c6cb371f94d85490f76597edd0
Nguyen The Minh <s3878434@rmit.edu.vn>
2021-09-10 00:56:13 +0700

    update admin link

--- php/admin.php
+++ php/admin.php
@@ -2,9 +2,8 @@
 <h1>Welcome Admin</h1>
 <li><a href="logout_admin.php">Log out</a></li>
   <li><a href="transactions.php">See Transaction</a></li>
-  <li><a href="contact.asp">See Balance</a></li>
-  <li><a href="about.asp">Undo Balance</a></li>
-  <li><a href="about.asp">Add Balance</a></li>
+  <li><a href="updatebalance.php">See Balance</a></li>
+
 </ul>
 
 
